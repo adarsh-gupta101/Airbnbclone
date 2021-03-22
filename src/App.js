d5e8a2563e7a51ff01cafd8d5f4f30728b8930ff
Adarsh <adarshgupta2468@gmail.com>
2021-03-22 22:47:31 +0530

    mnrerr

--- src/App.js
+++ src/App.js
@@ -23,33 +23,33 @@ function App() {
     <div className="App">
       <Header />
       <Banner />
-      <div className="card_section">
-        <Cards values={db} />;
+      <div className="card_section">{/* <Cards values={db} />; */}</div>
+      <div id="cdc" style={{ display: "flex" }}>
+        <Cards
+          src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
+          title="3 Bedroom Flat in Bournemouth"
+          description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
+          price="£130/night"
+        />
+        <Cards
+          src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
+          title="Penthouse in London"
+          description="Enjoy the amazing sights of London with this stunning penthouse"
+          price="£350/night"
+        />
+        <Cards
+          src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
+          title="Penthouse in London"
+          description="Enjoy the amazing sights of London with this stunning penthouse"
+          price="£350/night"
+        />
+        <Cards
+          src="https://media.nomadicmatt.com/2018/apartment.jpg"
+          title="1 Bedroom apartment"
+          description="Superhost with great amenities and a fabolous shopping complex nearby"
+          price="£70/night"
+        />
       </div>
-      <Cards
-        src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
-        title="3 Bedroom Flat in Bournemouth"
-        description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
-        price="£130/night"
-      />
-      <Cards
-        src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
-        title="Penthouse in London"
-        description="Enjoy the amazing sights of London with this stunning penthouse"
-        price="£350/night"
-      />
-      <Cards
-        src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
-        title="Penthouse in London"
-        description="Enjoy the amazing sights of London with this stunning penthouse"
-        price="£350/night"
-      />
-      <Cards
-        src="https://media.nomadicmatt.com/2018/apartment.jpg"
-        title="1 Bedroom apartment"
-        description="Superhost with great amenities and a fabolous shopping complex nearby"
-        price="£70/night"
-      />
       <div className="card_section">
         <Cards
           src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
